b85929828630f5c68ec2dff0a672b4f88b4df62f
Vexati-art <55270612+Vexati-art@users.noreply.github.com>
2020-09-06 12:59:53 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,129 +1,62 @@
-<?php
-
-echo "<!DOCTYPE html>\n";
-
-echo "<html>\n";
-
-echo "<head>\n";
-
-echo "<link rel=\"stylesheet\" href=\"Css/style.css\">\n";
-
-echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n";
-
-echo "\n";
-
-echo "</head>\n";
-
-echo "\n";
-
-echo "<body>\n";
-
-echo "	\n";echo "<div class=\"navbar\">\n";
-
-echo "	<img src=\"images/logo.jpeg\" alt=\"Paris\" width:\"65px\">\n";
-
-echo "  <a href=\"#home\">Home</a>\n";
-
-echo "  <a href=\"#news\">News</a>\n";
-
-echo "  <a href=\"#contact\">Contact</a>\n";
-
-echo "</div>\n";
-
-echo "<div id=\"tfheader\">\n";
-
-echo "		<form id=\"tfnewsearch\" method=\"get\" action=\"http://www.google.com\">\n";
-
-echo "		        <input type=\"text\" class=\"tftextinput\" name=\"q\" size=\"15\" maxlength=\"90\"><input type=\"submit\" value=\"Search\" class=\"tfbutton\">\n";
-
-echo "		</form>\n";
-
-echo "	<div class=\"tfclear\"></div>\n";
-
-echo "	</div>\n";
-
-echo "	<div class=\"row\">\n";
-
-echo "<div class=\"column\" style=\"background-color: yellow;\">\n";
-
-echo "    <marquee>  <h4>UPDATES</h4>  </marquee> \n";
-
-echo "    <p>hahahahaahahahahaahahaja\n";
-
-echo "jajahaahajajajaajajajaj\n";
-
-echo "    ahaaheheheheheh\n";
-
-echo "ejeejejhehejejehejeheh\n";
-
-echo "duduwhakfksh\n";
-
-echo "qjdkaodjakdiaoenwjs\n";
-
-echo "ajdjwjrksieh\n";
-
-echo "dkwjwhgwnejsjqjebqjd\n";
-
-echo "fsgwwgeggjrs hajakwiwkekwk\n";
-
-echo "xdjwiwodjsbekwkdkan\n";
-
-echo "hwjskekdjwkdjwjsj\n";
-
-echo "jwjeiwowjdhwushe\n";
-
-echo "jeiwieiwodjwkroekdhwkwirjww</p> \n";
-
-echo "            </div>\n";
-
-echo " <div class=\"column\" style=\"background-color: yellow;\">              \n";
-
-echo "    <marquee>  <h4>LATEST</h4>  </marquee>  \n";
-
-echo "      <p>hahahahaahahahahaahahaja\n";
-
-echo "jajahaahajajajaajajajaj\n";
-
-echo "    ahaaheheheheheh\n";
-
-echo "ejeejejhehejejehejeheh\n";
-
-echo "duduwhakfksh\n";
-
-echo "qjdkaodjakdiaoenwjs\n";
-
-echo "ajdjwjrksieh\n";
-
-echo "dkwjwhgwnejsjqjebqjd\n";
-
-echo "fsgwwgeggjrs hajakwiwkekwk\n";
-
-echo "xdjwiwodjsbekwkdkan\n";
-
-echo "hwjskekdjwkdjwjsj\n";
-
-echo "jwjeiwowjdhwushe\n";
-
-echo "jeiwieiwodjwkroekdhwkwirjww</p> \n";
-
-echo "            </div>\n";
-
-echo "           </div>\n";
-
-echo "<div class=\"footer\">\n";
-
-echo "  <p>Yellow Umbrella Corp.</p>\n";
-
-echo "</div>           \n";
-
-echo "\n";
-
-echo "</body>\n";
-
-echo "</html>";
-
-?>
-
-
-
-
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="Css/style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+	
+<div class="navbar">
+	<img src="images/logo.jpeg" alt="Paris" width:"65px">
+  <a href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+</div>
+<div id="tfheader">
+		<form id="tfnewsearch" method="get" action="http://www.google.com">
+		        <input type="text" class="tftextinput" name="q" size="15" maxlength="90"><input type="submit" value="Search" class="tfbutton">
+		</form>
+	<div class="tfclear"></div>
+	</div>
+	<div class="row">
+<div class="column" style="background-color: yellow;">
+    <marquee>  <h4>UPDATES</h4>  </marquee> 
+    <p>hahahahaahahahahaahahaja
+jajahaahajajajaajajajaj
+    ahaaheheheheheh
+ejeejejhehejejehejeheh
+duduwhakfksh
+qjdkaodjakdiaoenwjs
+ajdjwjrksieh
+dkwjwhgwnejsjqjebqjd
+fsgwwgeggjrs hajakwiwkekwk
+xdjwiwodjsbekwkdkan
+hwjskekdjwkdjwjsj
+jwjeiwowjdhwushe
+jeiwieiwodjwkroekdhwkwirjww</p> 
+            </div>
+ <div class="column" style="background-color: yellow;">              
+    <marquee>  <h4>LATEST</h4>  </marquee>  
+      <p>hahahahaahahahahaahahaja
+jajahaahajajajaajajajaj
+    ahaaheheheheheh
+ejeejejhehejejehejeheh
+duduwhakfksh
+qjdkaodjakdiaoenwjs
+ajdjwjrksieh
+dkwjwhgwnejsjqjebqjd
+fsgwwgeggjrs hajakwiwkekwk
+xdjwiwodjsbekwkdkan
+hwjskekdjwkdjwjsj
+jwjeiwowjdhwushe
+jeiwieiwodjwkroekdhwkwirjww</p> 
+            </div>
+           </div>
+<div class="footer">
+  <p>Yellow Umbrella Corp.</p>
+</div>           
+
+</body>
+</html>
